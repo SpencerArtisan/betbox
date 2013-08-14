a884d005173915386bce9ae0a2c32d554b368ad7
Spencer <spencerartisan@gmail.com>
2013-08-14 22:23:42 +0100

    Implemented lay odds strategy

---
+++ lib/lay_odds_strategy.rb
@@ -0,0 +1,19 @@
+#   Lay     Stake
+#A  a=2.12  x
+#B  b=2.06  1-x
+#If A loses, you win 1 - b(1-x)
+#If B loses, you win 1 - ax
+#For sure profit, 1-b(1-x)>0 and 1-ax>0
+#So, assuming 1-ax=0(x=1/a), then 1-b(1-1/a)>0
+class LayOddsStrategy
+  def initialize market
+    @market = market
+  end
+
+  def take_bet?
+    odds = @market.odds
+    factor = 1 - odds.player_b_lay * (1 - 1.0 / odds.player_a_lay)
+    puts "Factor is #{factor} (Lay odds are #{odds.player_a_lay} and #{odds.player_b_lay})"
+    factor >= 0
+  end
+end
---
+++ spec/lay_odds_strategy_spec.rb
@@ -0,0 +1,25 @@
+require 'lay_odds_strategy'
+
+describe LayOddsStrategy do
+  let (:odds) { double }
+  let (:market) { double odds: odds }
+  let (:strategy) { LayOddsStrategy.new market }
+
+  it 'should place a lay bet if the factor is zero' do
+    odds.stub player_a_lay: 2
+    odds.stub player_b_lay: 2
+    expect(strategy.take_bet?).to be_true
+  end
+
+  it 'should place a bet if the factor is above zero' do
+    odds.stub player_a_lay: 1.99
+    odds.stub player_b_lay: 1.99
+    expect(strategy.take_bet?).to be_true
+  end
+
+  it 'should not place a bet if the factor is below zero' do
+    odds.stub player_a_lay: 2.01
+    odds.stub player_b_lay: 2.01
+    expect(strategy.take_bet?).to be_false
+  end
+end
