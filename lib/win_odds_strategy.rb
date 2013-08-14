e6d8766541cdbd20367b4ce572986a0cc8636e47
Spencer <spencerartisan@gmail.com>
2013-08-14 21:52:58 +0100

    Minor rename\

--- lib/win_odds_strategy.rb
+++ lib/win_odds_strategy.rb
@@ -5,8 +5,8 @@ class WinOddsStrategy
 
   def take_bet?
     odds = @market.odds
-    ratio = (1 - 1.0 / odds.player_a_win) * odds.player_b_win
-    puts "Ratio is #{ratio} (Bet odds are #{odds.player_a_win} and #{odds.player_b_win})"
-    ratio >= 1
+    factor = (1 - 1.0 / odds.player_a_win) * odds.player_b_win
+    puts "Factor is #{factor} (Bet odds are #{odds.player_a_win} and #{odds.player_b_win})"
+    factor >= 1
   end
 end
