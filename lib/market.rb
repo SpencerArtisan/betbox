bff7905e782b6c06d7aaff7198914115ad088bf4
Spencer <spencerartisan@gmail.com>
2013-08-15 09:07:50 +0100

    Bug fix

--- lib/market.rb
+++ lib/market.rb
@@ -3,13 +3,13 @@ require 'ostruct'
 
 Odds = Struct.new :player_a_win, :player_b_win, :player_a_lay, :player_b_lay do
   def initialize prices
-    @player_a_win = prices[0][:b1]
-    @player_a_lay = prices[0][:l1]
-    @player_b_win = prices[1][:b1]
-    @player_b_lay = prices[1][:l1]
-    puts @player_a_win + "|" + @player_a_lay
-    puts @player_b_win + "|" + @player_b_lay
-    raise "Incomplete odds" if @player_a_win == 0 or @player_b_win == 0 or @player_a_lay == 0 or @player_b_lay == 0
+    self.player_a_win = prices[0][:b1]
+    self.player_a_lay = prices[0][:l1]
+    self.player_b_win = prices[1][:b1]
+    self.player_b_lay = prices[1][:l1]
+    puts "#{player_a_win}|#{player_a_lay}"
+    puts "#{player_b_win}|#{player_b_lay}"
+    raise "Incomplete odds" if player_a_win == 0 or player_b_win == 0 or player_a_lay == 0 or player_b_lay == 0
   end
 end
 
@@ -45,7 +45,7 @@ class Market
     from_date       = Time.now.utc
     to_date         = Time.now.utc
     @market = @bf.get_market @session_token, EXCHANGE_ID, @id, locale
-    raise "Market is INVALID" if @market.starts_with? 'INVALID_MARKET'
+    raise "Market is INVALID" if @market.to_s.starts_with? 'INVALID_MARKET'
     raise "Market is CLOSED" if @market[:market_status] == "CLOSED"
     puts "#{@market[:runners][:runner][0][:name]} vs #{@market[:runners][:runner][1][:name]}"
   end
